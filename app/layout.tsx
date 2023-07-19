447805fb83688ea4d2005dd3d0244cbc7c46b2a4
Wishtrader <mr.akomarov@gmail.com>
2023-07-20 00:30:16 +0300

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import localFont from 'next/font/local'
 import { Inter } from 'next/font/google'
+
+const sGrotesk = localFont({
+  src: './font/SpaceGrotesk-VariableFont_wght.ttf',
+  display: 'swap',
+})
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={sGrotesk.className}>{children}</body>
     </html>
   )
 }
